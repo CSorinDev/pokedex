b7176d7ad12939df37c4a34877b5554f41afeb30
Sorin C. <csorin.dev@gmail.com>
2026-02-24 13:35:13 +0100

    removed old pokemons page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
-import Pokemons from './pages/Pokemons'
 import Login from './pages/Login'
 import Layout from './layouts/Layout'
 import PokemonInfo from './pages/PokemonInfo'
 import Home from './pages/Home'
 import RegisterPage from './pages/RegisterPage'
 import MyProfile from './pages/MyProfile'
-import Pokemonsv2 from './pages/Pokemonsv2'
+import Pokemons from './pages/Pokemons'
+import PokemonsOld from './pages/Pokemons-old'
 
 function AppRoutes() {
   return (
@@ -14,8 +14,8 @@ function AppRoutes() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/pokemonsold" element={<PokemonsOld />} />
           <Route path="/pokemons" element={<Pokemons />} />
-          <Route path="/pokemonsv2" element={<Pokemonsv2 />} />
           <Route path="/pokemon/:id" element={<PokemonInfo />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<RegisterPage />} />
